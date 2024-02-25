f9ae1bc4124d2520de080a53497ae17692358a6e
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2024-02-25 14:50:08 +0100

    Add Register modal UI

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -69,7 +69,7 @@ const Modal: React.FC<ModalProps> = ({
   return (
     <>
       <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
-        <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full  md:h-auto lg:h-auto">
+        <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full md:h-auto lg:h-auto">
           {/* CONTENT */}
           <div
             className={`translate duration-300 h-full ${
@@ -78,7 +78,7 @@ const Modal: React.FC<ModalProps> = ({
           >
             <div className="translate h-full md:h-auto lg:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
               {/* HEADER */}
-              <div className="flex items-center p-6 rounded-t justify-center relative border-b-[1px]">
+              <div className="flex items-center p-4 rounded-t justify-center relative border-b-[1px]">
                 <button
                   onClick={handleClose}
                   className="p-1 border-0 hover:opacity-70 transition absolute left-9"
@@ -88,9 +88,9 @@ const Modal: React.FC<ModalProps> = ({
                 <div className="text-lg font-semibold">{title}</div>
               </div>
               {/* BODY */}
-              <div className="relative p-6 flex-auto">{body}</div>
+              <div className="relative p-4 flex-auto">{body}</div>
               {/* FOOTER */}
-              <div className="flex flex-col gap-2 p-6">
+              <div className="flex flex-col gap-2 p-4">
                 <div className="flex flex-row items-center gap-4 w-full">
                   {secondaryAction && secondaryActionLabel && (
                     <Button
