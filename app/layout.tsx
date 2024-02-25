95d431de7e6cb507536e11788901fc4114864049
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2024-02-25 11:24:39 +0100

    Add Modal and Button components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
